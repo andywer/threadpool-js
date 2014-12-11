a56b8e9e7737dbfa2d462a62e5c13ffd4ec79b62
Andy Wermke <andy@dev.next-step-software.com>
2014-12-11 21:08:20 +0100

    Changed indent from 4 to 2 spaces.

--- spec/threadpool.spec.js
+++ spec/threadpool.spec.js
@@ -2,40 +2,40 @@ require('jasmine-expect');
 
 describe('threadpool', function() {
 
-    it('has been initialzed', function() {
-        expect(ThreadPool).toBeFunction();
-    });
+  it('has been initialized', function() {
+    expect(ThreadPool).toBeFunction();
+  });
 
-    describe('workers', function() {
+  describe('workers', function() {
 
-        it('are spawned', function() {
-            var pool = new ThreadPool()
-              , actionCalled = 0
-              , done = false;
+    it('are spawned', function() {
+      var pool = new ThreadPool()
+        , actionCalled = 0
+        , done = false;
 
-            var action = function(param, done) {
-                done();
-            };
+      var action = function(param, done) {
+        done();
+      };
 
-            runs(function() {
-                pool.run(action);
-                pool.run(action);
-                
-                pool.done(function() {
-                    actionCalled++;
-                });
+      runs(function() {
+        pool.run(action);
+        pool.run(action);
 
-                setTimeout(function() {
-                    expect(actionCalled).toEqual(2);
-                    done = true;
-                }, 500);
-            });
-            
-            waitsFor(function() {
-                return done;
-            });
+        pool.done(function() {
+          actionCalled++;
         });
-        
+
+        setTimeout(function() {
+          expect(actionCalled).toEqual(2);
+          done = true;
+        }, 500);
+      });
+
+      waitsFor(function() {
+        return done;
+      });
     });
+
+  });
 
 });
