97868db42f4bb00bcf5687db2335df0977a8a08e
Andy Wermke <andy@dev.next-step-software.com>
2014-12-11 21:24:10 +0100

    Some more testing.

--- spec/threadpool.spec.js
+++ spec/threadpool.spec.js
@@ -36,6 +36,65 @@ describe('threadpool', function() {
       });
     });
 
+    it('trigger start event', function() {
+      var pool = new ThreadPool()
+        , startEvents = 0
+        , done = false;
+
+      var action = function(param, done) {
+        done();
+      };
+
+      runs(function() {
+        function startEventHandler() {
+          startEvents++;
+        }
+
+        pool.run(action).start(startEventHandler);
+        pool.run(action).start(startEventHandler);
+
+        pool.done(function() {
+          expect(startEvents).toEqual(2);
+          done = true;
+        });
+      });
+
+      waitsFor(function() {
+        return done;
+      });
+    });
+
+    it('trigger error event', function() {
+      var pool = new ThreadPool()
+        , errorEvents = 0
+        , poolError = 0
+        , done = false;
+
+      var action = function(param, done) {
+        throw new Error('Test');
+      };
+
+      runs(function() {
+        function errorEventHandler() {
+          errorEvents++;
+        }
+
+        pool.run(action).error(errorEventHandler);
+        pool.run(action).error(errorEventHandler);
+
+        pool.error(function() {
+          poolError++;
+
+          expect(errorEvents).toEqual(poolError);
+          if (errorEvents == 2) { done = true; }
+        });
+      });
+
+      waitsFor(function() {
+        return done;
+      });
+    });
+
   });
 
 });
