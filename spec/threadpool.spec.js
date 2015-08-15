8ce29e4acb7dc1b77ae654e61e4a26da91a13037
Andy Wermke <andy@dev.next-step-software.com>
2015-08-15 15:51:52 +0200

    New test cases and improved test performance

--- spec/threadpool.spec.js
+++ spec/threadpool.spec.js
@@ -1,8 +1,34 @@
 require('./helpers');
-require('../dist/threadpool.min.js');
+require('../dist/threadpool.js');
 
 var ThreadPool  = window.ThreadPool;
 var expect      = require('expect.js');
+
+
+/**
+ * Wait until condition is fulfilled or max waiting time has passed.
+ * @param {Function} conditionCb
+ * @param {Number} maxWaitMs
+ * @param {Function} fulfillCb
+ */
+function waitFor(conditionCb, maxWaitMs, fulfillCb) {
+  function repeatingCheck() {
+    if (conditionCb()) {
+      clearInterval(checkInterval);
+      clearTimeout(timeout);
+      fulfillCb(true);
+    }
+  }
+
+  function cancel() {
+    clearInterval(checkInterval);
+    fulfillCb(false);
+  }
+
+  var checkEveryMs = 10;
+  var checkInterval = setInterval(repeatingCheck, checkEveryMs);
+  var timeout = setTimeout(cancel, maxWaitMs);
+}
 
 
 describe('threadpool', function() {
@@ -14,8 +40,8 @@ describe('threadpool', function() {
   describe('workers', function() {
 
     it('are spawned', function(done) {
-      var pool = new ThreadPool()
-        , actionCalled = 0;
+      var pool = new ThreadPool();
+      var actionCalled = 0;
 
       var action = function(param, actionDone) {
         actionDone();
@@ -28,16 +54,16 @@ describe('threadpool', function() {
         actionCalled++;
       });
 
-      setTimeout(function() {
+      waitFor(function() { return actionCalled === 2; }, 500, function() {
         expect(actionCalled).to.equal(2);
         done();
-      }, 500);
+      });
     });
 
     it('trigger start event', function(done) {
-      var pool = new ThreadPool()
-        , startEvents = 0
-        , doneCalled = false;
+      var pool = new ThreadPool();
+      var startEvents = 0;
+      var doneCalled = false;
 
       var action = function(param, actionDone) {
         actionDone();
@@ -60,11 +86,11 @@ describe('threadpool', function() {
     });
 
     it('trigger error event', function(done) {
-      var pool = new ThreadPool()
-        , errorEvents = 0
-        , poolError = 0;
+      var pool = new ThreadPool();
+      var errorEvents = 0;
+      var poolError = 0;
 
-      var action = function(param, done) {
+      var action = function() {
         throw new Error('Test');
       };
 
@@ -83,14 +109,66 @@ describe('threadpool', function() {
       });
     });
 
+    it('can work asynchronously', function(done) {
+      var pool = new ThreadPool();
+      var actionCalled = 0;
+
+      var action = function(param, actionDone) {
+        thread.nextTick(actionDone);
+      };
+
+      pool.run(action);
+      pool.run(action);
+
+      pool.done(function() {
+        actionCalled++;
+      });
+
+      waitFor(function() { return actionCalled === 2; }, 500, function() {
+        expect(actionCalled).to.equal(2);
+        done();
+      });
+    });
+
+    it('can be re-used', function(done) {
+      // only two threads
+      var pool = new ThreadPool(2);
+      var actionCalled = 0;
+
+      var action = function(param, actionDone) {
+        thread.nextTick(function() {
+          actionDone();
+        });
+      };
+
+      // queueing more jobs than there are threads
+      pool.run(action);
+      pool.run(action);
+      pool.run(action);
+
+      pool.done(function() {
+        actionCalled++;
+      });
+
+      // TODO: Spy on WorkerFactory to make sure there are only two threads spawned
+
+      pool.allDone(function() {
+        try {
+          expect(actionCalled).to.equal(3);
+        } catch (error) {
+          return done(error);
+        }
+        done();
+      });
+    });
+
   });
 
 
   it('allDone() is triggered once on overall completion', function(done) {
-
-    var pool = new ThreadPool()
-      , actionCalled = 0
-      , allDoneCalled = 0;
+    var pool = new ThreadPool();
+    var actionCalled = 0;
+    var allDoneCalled = 0;
 
     var action = function(param, actionDone) {
       actionDone();
@@ -105,13 +183,12 @@ describe('threadpool', function() {
 
     pool.allDone(function() {
       allDoneCalled++;
-    })
+    });
 
-    setTimeout(function() {
+    waitFor(function() { return allDoneCalled === 1; }, 500, function() {
       expect(allDoneCalled).to.equal(1);
       done();
-    }, 500);
-
+    });
   });
 
 });
